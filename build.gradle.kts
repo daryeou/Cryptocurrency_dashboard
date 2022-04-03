8297a2aa9bff5a0d92bf6d6521eb184ac60567e1
harusiku <harusiku0610@gmail.com>
2022-04-03 22:44:04 +0900

    Refactor Gradle: Root gradle Kotlin-DSL로 변경 및 프로가드 추가

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
+}
